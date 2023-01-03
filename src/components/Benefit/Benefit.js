afcb5e11510883d40b24cee569c5b2c41ddcb466
MD HABIBUL BASAR SHOHAG <mdhabibulbasarshohag@MDs-MacBook-Air.local>
2023-01-03 22:12:06 +0600

    first commit

---
+++ src/components/Benefit/Benefit.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import './Benefit.css';
+
+const Benefit = () => {
+    return (
+        <div className='my-5'>
+            <div className='container'>
+                <div>
+                    <h1 className='text-center fw-bold mb-3'>Travel Benefit For User</h1>
+                    <div className='row justify-content-center'>
+                        <div className='col-lg-6 col-md-10'>
+                            <p className='text-center'>
+                                We are self-service data analytics software that lets you create visually appealing  data visualizations and insightful dashboards in minutes.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='row'>
+                        <div className='col-lg-4 col-md-6 col-12 my-3'>
+                            <div className='p-5 cart-body rounded-3'>
+                                <div>
+                                    <img className='mb-3' src="img/Benefit.png" alt="" />
+                                </div>
+                                <div>
+                                    <h4 className='mb-2'>Cheap than Other</h4>
+                                    <p>
+                                        Travelya is cheaper than other
+                                        travel agency.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-4 col-md-6 col-12 my-3'>
+                            <div className='p-5 cart-body rounded-3'>
+                                <div>
+                                    <img className='mb-3' src="img/Benefit2.png" alt="" />
+                                </div>
+                                <div>
+                                    <h4 className='mb-2'>Cheap than Other</h4>
+                                    <p>
+                                        Travelya is cheaper than other
+                                        travel agency.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-4 col-md-6 col-12 my-3'>
+                            <div className='p-5 cart-body rounded-3'>
+                                <div>
+                                    <img className='mb-3' src="img/Benefit3.png" alt="" />
+                                </div>
+                                <div>
+                                    <h4 className='mb-2'>Cheap than Other</h4>
+                                    <p>
+                                        Travelya is cheaper than other
+                                        travel agency.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-4 col-md-6 col-12 my-3'>
+                            <div className='p-5 cart-body rounded-3'>
+                                <div>
+                                    <img className='mb-3' src="img/Benefit4.png" alt="" />
+                                </div>
+                                <div>
+                                    <h4 className='mb-2'>Cheap than Other</h4>
+                                    <p>
+                                        Travelya is cheaper than other
+                                        travel agency.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-4 col-md-6 col-12 my-3'>
+                            <div className='p-5 cart-body rounded-3'>
+                                <div>
+                                    <img className='mb-3' src="img/Benefit5.png" alt="" />
+                                </div>
+                                <div>
+                                    <h4 className='mb-2'>Cheap than Other</h4>
+                                    <p>
+                                        Travelya is cheaper than other
+                                        travel agency.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-4 col-md-6 col-12 my-3'>
+                            <div className='p-5 cart-body rounded-3'>
+                                <div>
+                                    <img className='mb-3' src="img/Benefit6.png" alt="" />
+                                </div>
+                                <div>
+                                    <h4 className='mb-2'>Cheap than Other</h4>
+                                    <p>
+                                        Travelya is cheaper than other
+                                        travel agency.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Benefit;
